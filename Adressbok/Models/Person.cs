3d10a11cc9906354d5621ef2abb2bbf790f156b0
kalle (jag) <kalle@jillheden.se>
2017-11-02 14:43:04 +0100

    Removed redundant using satements

--- Adressbok/Models/Person.cs
+++ Adressbok/Models/Person.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using System.Xml.XPath;
 using Adressbok.DAL;
 
 namespace Adressbok.Models
